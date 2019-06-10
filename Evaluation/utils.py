fe86b969afb779a38df9675092882503d6129e79
Shi Tian <shitian@taiger.com>
2019-06-10 14:33:23 +0800

    Add utils for type checking

---
+++ Evaluation/utils.py
@@ -0,0 +1,18 @@
+from enum import IntEnum
+from typing import Dict, List, Tuple, Callable
+
+class TessType(IntEnum):
+    OCR_PAGE = 0
+    OCR_AREA = 1
+    OCR_PAR = 2
+    OCR_LINE = 3 # OCR_CAPTION, OCR_HEADER, OCR_TEXTFLOAT are on the same indentation level
+    OCRX_WORD = 4
+
+class AbbyType(IntEnum):
+    PAGE = 0
+    TABLE = 1
+    T_ROW = 2
+    P = 3
+    LINE = 4
+    WORD = 5
+    CHAR = 6
